6dacb6e2253a269bb3bebe62d810715059fb6bd8
k47ma <k47ma@edu.uwaterloo.ca>
2018-05-23 20:55:26 -0400

    Restructured files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from algorithm import Algorithm
+from lib.algorithm import Algorithm
 
 if __name__ == '__main__':
     algo = Algorithm(population_size=100, mutation_rate=0.1, max_generation=10000)
